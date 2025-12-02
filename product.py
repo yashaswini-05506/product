aef8db2f1b96c2d3953140ea0a45d43471a01240
yashaswini-05506 <yashaswinimadnallimath@gmail.com>
2025-12-02 10:26:14 +0530

    Update product.py

--- product.py
+++ product.py
@@ -1,16 +1,16 @@
-def product_details(name, id, quantity, price):
-    result = (
-        f"Product Name: {name}\n"
-        f"Product ID: {id}\n"
-        f"Quantity: {quantity}\n"
-        f"Price: {price}"
+def product_details(name,product_id, quantity, price):
+    result=(
+        f"Product Name:{name}\n"
+        f"Product ID:{product_id}\n"
+        f"Quantity:{quantity}\n"
+        f"Price:{price}"
     )
     return result
 
-if __name__ == "__main__":
+if __name__=="__main__":
     # Sample Output
-    name = "Lenovo Laptop"
-    id = "P1001"
-    quantity = 10
-    price = 48000
-    print(product_details(name, id, quantity, price))
+    name="Lenovo Laptop"
+    id="P1001"
+    quantity=10
+    price=48000
+    print(product_details(name,product_id,quantity,price))
