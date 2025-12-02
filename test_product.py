906a5e5ad022cedb2caa252ffdfffb05a291593f
yashaswini-05506 <yashaswinimadnallimath@gmail.com>
2025-12-02 10:28:43 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -1,10 +1,10 @@
 from product import product_details
 
 def test_product_details():
-    expected_output = (
-        "Product Name: Lenovo Laptop\n"
-        "Product ID: P1001\n"
-        "Quantity: 10\n"
-        "Price: 48000\n"
+    expected_output=(
+        "Product Name:Lenovo Laptop\n"
+        "Product ID:P1001\n"
+        "Quantity:10\n"
+        "Price:48000"
     )
-    assert product_details("Lenovo Laptop", "P1001", 10, 48000) == expected_output
+    assert product_details("Lenovo Laptop", "P1001", 10, 48000)==expected_output
